994623b302fbd0b83eb60ad8430e27753c617e4b
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2024-07-23 13:20:49 -0400

    finished main workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Blue from './components/Blue.jsx'
+import Red from './components/Red.jsx'
 
 const App = () => {
   
 
   return (
-    <>
-      
-    </>
+    <div id="container">
+
+      <h1>Hello React Router!</h1>
+
+      <div id="navbar">
+        <Link to='/'>Home</Link>
+        <Link to='/blue'>Blue</Link>
+        <Link to="/red">Red</Link>
+      </div>
+
+      <div id="main-section">
+
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/blue' element={<Blue />} />
+          <Route path='/red' element={<Red />} />
+          {/*TODO   <Route path='/:color' />   TODO*/}
+        </Routes>
+
+      </div>
+
+    </div>
   )
 }
 
